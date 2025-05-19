ecdd40ad7e53086ce7a4dbfba8a7c3e30d0f923a
Chen Kai <281165273grape@gmail.com>
2025-05-19 21:57:10 +0800

    Revert "make send in separate goroutine"
    This reverts commit 62d6517c06ea921a69e058e8e8a79464493f1e4d.

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -37,7 +37,6 @@ import (
 	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/ethereum/go-ethereum/p2p/enr"
 	"github.com/ethereum/go-ethereum/p2p/netutil"
-	bufferpool "github.com/libp2p/go-buffer-pool"
 )
 
 const (
@@ -100,7 +99,6 @@ type UDPv5 struct {
 	sendCh        chan sendRequest
 	sendNoRespCh  chan *sendNoRespRequest
 	unhandled     chan<- ReadPacket
-	writeCh       chan pendingWrite // New channel for outgoing packets
 
 	// state of dispatch
 	codec            codecV5
@@ -114,12 +112,6 @@ type UDPv5 struct {
 	closeCtx       context.Context
 	cancelCloseCtx context.CancelFunc
 	wg             sync.WaitGroup
-}
-
-// pendingWrite holds data for a packet to be sent by the writeLoop.
-type pendingWrite struct {
-	toAddr netip.AddrPort
-	data   []byte
 }
 
 type sendRequest struct {
@@ -166,10 +158,9 @@ func ListenV5(conn UDPConn, ln *enode.LocalNode, cfg Config) (*UDPv5, error) {
 		return nil, err
 	}
 	go t.tab.loop()
-	t.wg.Add(3)
+	t.wg.Add(2)
 	go t.readLoop()
 	go t.dispatch()
-	go t.writeLoop()
 	return t, nil
 }
 
@@ -197,7 +188,6 @@ func newUDPv5(conn UDPConn, ln *enode.LocalNode, cfg Config) (*UDPv5, error) {
 		sendNoRespCh:  make(chan *sendNoRespRequest),
 		respTimeoutCh: make(chan *callTimeout),
 		unhandled:     cfg.Unhandled,
-		writeCh:       make(chan pendingWrite, 128), // Buffered channel for outgoing packets
 		// state of dispatch
 		codec:            v5wire.NewCodec(ln, cfg.PrivateKey, cfg.Clock, cfg.V5ProtocolID),
 		activeCallByNode: make(map[enode.ID]*callV5),
@@ -641,7 +631,6 @@ func (t *UDPv5) dispatch() {
 
 		case <-t.closeCtx.Done():
 			close(t.readNextCh)
-			close(t.writeCh)
 			for id, queue := range t.callQueue {
 				for _, c := range queue {
 					c.err <- errClosed
@@ -781,55 +770,10 @@ func (t *UDPv5) send(toID enode.ID, toAddr netip.AddrPort, packet v5wire.Packet,
 		t.log.Warn(">> "+packet.Name(), t.logcontext...)
 		return nonce, err
 	}
-
 	t.logcontext = append(t.logcontext, "rawpacket", hexutil.Encode(enc))
+	_, err = t.conn.WriteToUDPAddrPort(enc, toAddr)
 	t.log.Trace(">> "+packet.Name(), t.logcontext...)
-
-	dataForSend := bufferpool.Get(len(enc))
-	copy(dataForSend, enc)
-
-	pw := pendingWrite{
-		toAddr: toAddr,
-		data:   dataForSend, // codec.Encode should return a new slice, safe to pass directly.
-	}
-
-	select {
-	case t.writeCh <- pw:
-		// Packet successfully queued.
-		return nonce, nil
-	case <-t.closeCtx.Done():
-		bufferpool.Put(dataForSend)
-		return nonce, errClosed
-	}
-}
-
-// writeLoop runs in its own goroutine and sends packets from the writeCh to the network.
-func (t *UDPv5) writeLoop() {
-	defer t.wg.Done()
-	for pw := range t.writeCh { // Loop continues until writeCh is closed and empty.
-		_, err := t.conn.WriteToUDPAddrPort(pw.data, pw.toAddr)
-		if err != nil {
-			// Generic error logging, as we don't have packetName or rich context here.
-			select {
-			case <-t.closeCtx.Done():
-				// Log trace level if error occurs during or after shutdown initiation.
-				t.log.Trace("UDP write error during/after shutdown", "addr", pw.toAddr, "err", err)
-			default:
-				// Not closing, so it's a more unexpected error.
-				if netutil.IsTemporaryError(err) {
-					t.log.Debug("Temporary UDP write error", "addr", pw.toAddr, "err", err)
-				} else if !errors.Is(err, net.ErrClosed) && !errors.Is(err, io.EOF) { // Avoid logging common "closed" errors if not caught by closeCtx.
-					t.log.Warn("UDP write error", "addr", pw.toAddr, "err", err)
-				}
-			}
-		} else {
-			// Minimal trace log confirming the actual send.
-			// The detailed packet-specific trace was done in the 'send' method.
-			t.log.Trace("UDP packet data sent", "addr", pw.toAddr, "len", len(pw.data))
-		}
-
-		bufferpool.Put(pw.data)
-	}
+	return nonce, err
 }
 
 // readLoop runs in its own goroutine and reads packets from the network.
